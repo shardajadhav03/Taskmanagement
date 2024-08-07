3eec136a6964dc271aee8e0c65f3167ea530508f
Sharda Jadhav <110457645+shardajadhav03@users.noreply.github.com>
2024-08-07 17:30:49 +0530

    Update urls.py

--- taskmanagement/urls.py
+++ taskmanagement/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
 urlpatterns = [
-    path('',include('taskmanage.urls')),
+    path('home/',include('taskmanage.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
